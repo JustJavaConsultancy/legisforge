10f8afb78de8b901ddacfd059e04562bc3ad7080
Akinkunmi <akinrindeakinkunmi2006@gmail.com>
2025-11-11 17:41:18 +0100

    Process starting successfully

--- src/main/java/com/justjava/legisForge/projects/ProjectController.java
+++ src/main/java/com/justjava/legisForge/projects/ProjectController.java
@@ -1,10 +1,31 @@
 package com.justjava.legisForge.projects;
 
+import com.justjava.legisForge.aau.AuthenticationManager;
+import jakarta.servlet.http.HttpServletRequest;
+import org.flowable.engine.HistoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.history.HistoricProcessInstance;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ProjectController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    HistoryService historyService;
+
     @GetMapping("/projects")
     public String getProjects() {
         return "projects/allProjects";
@@ -12,5 +33,45 @@ public class ProjectController {
     @GetMapping("/project-info")
     public String getProjectInfo() {
         return "projects/project-info";
+    }
+    @PostMapping("/create-project")
+        public String startProject(@RequestParam Map<String,Object> startVariables, Model model, HttpServletRequest
+        request) {
+
+        System.out.println(" The Sent Parameter Here === " + startVariables);
+
+        String businessKey = String.valueOf(authenticationManager.get("sub"));
+        startVariables.put("progress", 0);
+
+        ProcessInstance processInstance = runtimeService
+                .startProcessInstanceByKey("mergerAndAquisition", businessKey, startVariables);
+
+
+        System.out.println("Process started successfully with ID: "
+                + processInstance.getProcessInstanceId()
+                + " and BusinessKey: " + processInstance.getBusinessKey());
+
+        List<ProcessInstance> projects = runtimeService
+                .createProcessInstanceQuery()
+                .processDefinitionKey("mergerAndAquisition")
+                .includeProcessVariables()
+                .active()
+                .list();
+
+        System.out.println(" The Total Number of Active Process Instances ===" + projects.size());
+        List<HistoricProcessInstance> completedProcess = historyService
+                .createHistoricProcessInstanceQuery()
+                .finished()
+                .orderByProcessInstanceEndTime()
+                .desc()
+                .list();
+
+        model.addAttribute("projects", projects);
+        model.addAttribute("completedProject", completedProcess.size());
+        model.addAttribute("activeProject", projects.size());
+
+        // Get the page the request came from
+        String referer = request.getHeader("Referer");
+        return "redirect:" + (referer != null ? referer : "/");
     }
 }
